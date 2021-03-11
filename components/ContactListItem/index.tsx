39e04935f6b9fa9c1118395708454e08f8199287
Mira Heckmann <miraheckmann@gmail.com>
2021-03-11 15:44:04 -0800

    create chat room

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -3,7 +3,11 @@ import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import { User } from '../../types';
 import styles from './style';
 import moment from 'moment';
-import {useNavigation} from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { createChatRoom, createChatRoomUser } from '../../src/graphql/mutations';
+
  
 export type ContactListItemProps = {
     user: User
@@ -13,9 +17,55 @@ const ContactListItem = (props: ContactListItemProps) => {
     const { user } = props;
     const navigation = useNavigation();
     
-    const onClick = () => {
-  
+    const onClick = async () => {
+        try {
+            const newChatRoomData = await API.graphql(
+                graphqlOperation(
+                    createChatRoom, {
+                        input: {}
+                    }
+                )
+            )
+
+            if (!newChatRoomData.data) {
+                console.log("Failed to create")
+                return 
+            }
+            const newChatRoom = newChatRoomData.data.createChatRoom;
+            console.log(newChatRoom);
+                await API.graphql(
+                    graphqlOperation(
+                        createChatRoomUser, {
+                            input: {
+                                userID: user.id,
+                                chatRoomID: newChatRoom.id
+                            }
+                        }
+                    )
+                )
+            const userInfo = await Auth.currentAuthenticatedUser(); 
+                await API.graphql(
+                    graphqlOperation(
+                        createChatRoomUser, {
+                            input: {
+                                userID: userInfo.attributes.sub,
+                                chatRoomID: newChatRoom.id
+                                
+                            }
+                        }
+                    )
+                )
+            navigation.navigate('ChatRoom', {
+                id: newChatRoom.id,
+                name: "name",
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
     }
+
+ 
 
     return (
         <TouchableWithoutFeedback onPress={onClick}>
